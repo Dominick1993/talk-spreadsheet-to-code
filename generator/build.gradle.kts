ac6e9fe58f6969904bae18eb4bb10073adacfdaa
Dominik Labuda <dominikla@gmail.com>
2022-10-11 23:27:43 +0200

    Add detekt to the project

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -1,18 +1,34 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
 
+    idea
     application
+
+    alias(detektCatalog.plugins.detekt)
 }
 
 repositories {
     mavenCentral()
 }
 
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
+
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    detektPlugins(detektCatalog.formatting)
 }
 
 application {
     mainClass.set("dev.labuda.spreadsheets.AppKt")
+}
+
+detekt {
+    config = rootProject.files("detekt.yml")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    jvmTarget = JavaVersion.VERSION_17.majorVersion
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "turning-spreadsheets-into-code"
 include("generator")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        register("detektCatalog") {
+            val detektVersion = "1.21.0"
+            library("formatting", "io.gitlab.arturbosch.detekt", "detekt-formatting").version(detektVersion)
+            plugin("detekt", "io.gitlab.arturbosch.detekt").version(detektVersion)
+        }
+    }
+}
